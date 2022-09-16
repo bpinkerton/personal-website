906fea5155ef60f84ab9a4a641f8c4a0ecec18e8
Brandon Pinkerton <brandon.a.pinkerton@gmail.com>
2022-09-16 09:48:13 -0500

    add tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
-    <div>Hello world!</div>
+    <h1 className={"text-3xl font-bold underline"}>Hello world!</h1>
   )
 }
